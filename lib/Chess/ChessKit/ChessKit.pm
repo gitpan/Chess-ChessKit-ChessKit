34f398fcdd94a52cabf302f2ad1aa680ec9eca29
Charles MINC <charles.minc@wanadoo.fr>
2007-12-23 15:40:58 +0000

    Import of CHESSKIT/Chess-ChessKit-ChessKit-0.05 from CPAN.
    gitpan-cpan-distribution: Chess-ChessKit-ChessKit
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         CHESSKIT/Chess-ChessKit-ChessKit-0.05.tar.gz
    gitpan-cpan-author:       CHESSKIT
    gitpan-cpan-maturity:     released

--- lib/Chess/ChessKit/ChessKit.pm
+++ lib/Chess/ChessKit/ChessKit.pm
@@ -1,107 +1,293 @@
-package Chess::ChessKit::ChessKit;
 
-use warnings;
-use strict;
+
+package Chess::ChessKit;
+
 
 =head1 NAME
 
-Chess::ChessKit::ChessKit - The great new Chess::ChessKit::ChessKit!
+Chess::ChessKit  A set of program to translate chess notations and analyses game by moves count
+                 
 
 =head1 VERSION
 
-Version 0.01
+Version 0.04
 
 =cut
 
-our $VERSION = '0.01';
-
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+ChessKit is composed of:
 
-Perhaps a little code snippet.
+=over
 
-    use Chess::ChessKit::ChessKit;
+=item
 
-    my $foo = Chess::ChessKit::ChessKit->new();
-    ...
+Move.pm : basic moves on an empty board.
 
-=head1 EXPORT
+=item
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+Board.pm: functions for valid moves in a true game.
 
-=head1 FUNCTIONS
+=item
 
-=head2 function1
+Trad::trad : functions to translate algebraic from one language 
+             to another one
 
-=cut
+=item
 
-sub function1 {
-}
+Trad::dscrip :Translate english descriptive notation to 
+              long english algebraic
 
-=head2 function2
+=item
 
-=cut
+tradfromto.pl: standalone with a GUI (Tk/perl) using Trad::trad.
 
-sub function2 {
-}
+=item
 
-=head1 AUTHOR
+dscripdata.pl: example using Trad::dscrip and checks distribution
 
-Charles Minc, C<< <charles.minc at wanadoo.fr> >>
+=item
 
-=head1 BUGS
+anagraf.pl: example using Chess::ChessKit:Board to find "best" moves 
+              according to TMN with GUI
+	      
+=item
 
-Please report any bugs or feature requests to C<bug-chess-chesskit-chesskit at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Chess-ChessKit-ChessKit>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Chess::ChessKit::ChessKit
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Chess-ChessKit-ChessKit>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Chess-ChessKit-ChessKit>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Chess-ChessKit-ChessKit>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Chess-ChessKit-ChessKit>
+analizmov.pl: example using Chess::ChessKit:Board to find "best" moves 
+              according to TMN
 
 =back
 
+note: TMN : Theoretical move number : it's the precise number of different 
+valid moves at the disposal of the chess player having to play.
+
+=head1 DESCRIPTION
+
+ChessKit is a set of programs and packages 
+to translate any chess notation into
+another one.
+As a consequence we have to provide software
+that can validate pieces (or pawns) moves.
+
+About chess theory somebody states that advantage
+belongs to the player who has the bigger number
+of moves. So, to help to verify and pursue the idea,routines for that purpose 
+have been developped.
+
+=head1 EXPORT
+
+all the functions below here.
+
+
+=head1 FUNCTIONS inside Chess::ChessKit::Board
+
+
+
+B<$gmv{$piece}(row=>>$somerow,
+B<             col =>>$somecolumn,
+B<             mov=>>$ref_array_of_mov,
+B<             color=>>$piece) ;
+
+return in $ref_array_of_mov all the moves that $piece written like
+in FEN (for example 'N' for White Knight, 'n' for Black Knight and so on).
+($somerow,$somecolumn) are the coordinates of the starting square.
+color is again $piece because all the Whites are in upper case and blacks
+in lower case.
+
+
+B<$bd->>B<bestmove> ; print a list of the best moves in the sense of TMN
+
+B<$bd->>B<boardcopy($new_board)> ; 
+copy the position of board $bd to the other board $new_board
+
+B<$diag=$bd->>B<can_castling(roq=>>$roq  ,
+B<                     status=>>$ref_status, 
+B<                     out=>>$out) ;
+check if a king can castle according to $ref_status (updated by 
+has_moved). Also, verify the king is not checked.
+$roq=(KK|QK|kk|qk) where the first letter design the side and
+the second the king color. 
+$out=('yes'|'no')
+if $out is to 'yes' a message indicates which pieces
+give a check. Default is 'no'. 
+This function return 'yes' or 'no'.
+
+B<$bd->>B<cantake(who=>>$piece,
+B<             where=>>$some_square) ;
+return 'no' if not possible or the type of the piece that can be taken.
+
+B<$bd->>B<castling(couleur=>>$couleur,
+B<                side=>>$side,
+B<                status=>>$ref_status,
+B<                out=>>$out) 
+execute castling if can_castling return 'yes' or die with diagnostic.
+Parameters can take the following values:
+
+=over
+
+=item
+$couleur=('White|'Black')
+
+=item
+$side(K|Q)
+
+=item
+$ref_status is the parameter already described above.
+
+=item
+$out=('yes'|'no')
+transmit this data to can_castling which print which piece
+give a check. Default is 'no'.
+
+=back
+
+B<my ($cnt_white,$cnt_black)=$bd->>B<chessmovcnt>
+scan the board and return the count of the valid moves for both
+Blacks and Whites. (used next program-maintain for backward
+compatibility
+
+B<my %sum=$bd->>B<chessmovcntint>
+scan the board and return the set of squares of the valid moves for 
+Blacks @{$sum{b}} and Whites  @{$sum{w}}.
+We can retrieve the datas as before for the above routine 
+by the following way:
+
+	$cnt_white=@{$sum{w}} ;
+	$cnt_black=@{$sum{b}} ;
+
+Note: squares could be duplicated in the hash
+
+B<$bd->>B<chessview>
+print the board on screen.
+
+B<$bd->>B<deletepiece($location)>
+delete the piece at location $location that are 
+the algebraic coordinate of a square.
+
+B<$piece=$bd->>B<getpiece($location)>
+retrieve the type of piece at board location $location
+
+B<has_moved(status=>>$ref_status,
+B<             ini=>>$ini)>
+set the status of Rook and king to know if they are
+always at there original place. So this function 
+must be called after every moves.
+$ini=('y'|'no') if this parameter is set to 'y' the status is reset.
+The default option is of course 'no'.
+
+B<bd->>B<is_shaked(king=>>$king,
+B<                 out=>>$out)
+Diagnose if a king is checked by returning 'yes' or "" 
+$out conditional parameter for printing the result if equal 'y'
+
+B<$bd=Board->>B<new();>
+create the objet $bd of class Board.
+
+B<$bd->>B<print>
+print the position of pieces: location and type
+
+B<$bd->>B<put($piece,$location)>
+place a piece $piece on the chessboard at $location
+
+B<$bd->>B<startgame(%set_position)>
+initialize the chessboard with %set_position. Default without argument
+is the usual chessboard starting game.
+
+
+B<$bd->>B<valid(mov=>>$set_of move,
+B<              valid=>>$set,
+B<              piece=>>$piece)
+intermediate validation of moves. Use
+datas obtained from %gmv : $set_of_move for $piece.
+It returns $set , the moves restricted to the true board.
+
+
+B<$bd->>B<vldmov(valid=>>$set,
+B<               piece=>>$piece,
+                 row=>>$row,
+                 col=>>$col,
+                 recur=>>$control)
+Final validation of moves, where
+all the rules of chessgame are taken under consideration.
+Castling is separated and described before.
+
+B<$bd->>B<getfen($fen,$posini,$status)>
+where $fen is the FEN string and $posini a hash reference
+used to initialize the game by :
+
+	$bd->startgame($posini)
+
+$status is the status game as described before.
+
+B<$bd->>B<genfen($colmove,$status,$ep,$ply,$nmove)>
+genfen is the inverse function of getfen. It returns
+the FEN string if we provides the classical chess data
+of the colors who had to play $colmove ('w' or 'b'),
+the status $status set by $bd->hasmoved($status),
+ the column of the pawn if the taken "en passant"
+is possible $ep,the ply count $ply and at last, 
+the game move count $nmove.
+
+=head1 FUNCTIONS inside Chess::ChessKit::Trad
+
+
+B<trad($outfile,$infile,$lang ,$lang2translate)>
+translate an algebraic file or pgn given by $infile to $outfile
+where the source language is $lang and the output language is $lang2translate.
+They can take the following values:
+Czech|Danish|Dutch|English|Estonian|Finnish|French|German|Hungarian|
+Icelandic|Italian|Norwegian|Polish|Portuguese|Romanian|Spanish|Swedish
+
+B<@array_of_moves=dscrip(mov=>>$ref_array_of_moves ,
+B<                       ini=>>$ref_hash_position)
+Translate a descriptive notation with the following format:
+
+=over
+
+=item
+  n. Whitemove BlackMove
+
+=back
+
+where n is the move number followed by a dot then by the
+moves.(Note: last move of course could be only a white move)
+The input is a reference to the array of the moves in decriptive notation,
+An array in long algebraic notation is returned.
+If a $ref_hash_position is given then the global variable $bd is 
+set to those values. By default, it is again the initial chess game position. 
+
+
+=head1 SEE ALSO
+
+Chess::PGN,Chess::Board
+
+=head1 AUTHOR
+
+Charles Minc, C<< <charles.minc@wanadoo.fr> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-mymove@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Chess::ChessKit::ChessKit>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
 
 =head1 ACKNOWLEDGEMENTS
 
+L<http://www.chess-theory.com//enthnct03_1_new_chess_theory_unfolding_game.htm>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2007 Charles Minc, all rights reserved.
+Copyright 2005 Charles Minc, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
 
-1; # End of Chess::ChessKit::ChessKit
+1; # End of Chess::ChessKit
+
+
--- lib/Chess/ChessKit/Move.pm
+++ lib/Chess/ChessKit/Move.pm
@@ -1,107 +1,304 @@
-package Chess::ChessKit::Move;
+#!/usr/bin/perl -w
+####-----------------------------------
+### File	: Move.pm
+### Author	: Ch.Minc
+### Purpose	: Package for ChessKit
+### Version	: 1.1 2005/8/10
+### copyright GNU license
+####-----------------------------------
+
+package Move ;
+#package Chess::ChessKit::Move;
 
 use warnings;
 use strict;
 
+our $VERSION = '1.1';
+
 =head1 NAME
 
-Chess::ChessKit::Move - The great new Chess::ChessKit::Move!
+ChessKit::Move
 
 =head1 VERSION
 
-Version 0.01
+Version 1.1
 
 =cut
-
-our $VERSION = '0.01';
-
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Chess::ChessKit::Move;
-
-    my $foo = Chess::ChessKit::Move->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
+see ChessKit
 
 =cut
 
-sub function1 {
+# remainder How are built the moves
+#tour
+#iter=>7 dir =>3
+#my @inc_l=(0,0,-1,1) ;
+#my @inc_c=(-1,1,0,0) ;
+
+#fou 
+#iter=>7 dir =>3
+#my @inc_l=(1,1,-1,-1) ;
+#my @inc_c=(1,-1,-1,1) ;
+
+#cavalier 
+#iter=>0 dir =>7
+#my @inc_l=(-2,-2,2,2,-1,1,-1,1) ;
+#my @inc_c=(-1,1,-1,1,-2,-2,2,2) ;
+
+#dame= fou+tour
+#iter=>7 dir =>3
+
+#roi=fou+tour
+#iter=>0 dir =>3
+
+#pion
+#iter=>1 dir =>1
+#my @inc_l=(0) ;
+#my @inc_c=(1) ;
+
+#iter=>0 dir =>1 cds prise
+#my @inc_l=(1,1) ;
+#my @inc_c=(1,-1) ;
+
+#en_passant
+#alpha7-alpha5 || alpha2-alpha4
+
+#queenroq
+#states a1 || a8 && king + echec c1-d1-e1
+#kingroq
+#states h1 || h8 && king + echec       e1-f1-g1
+
+require Exporter ;
+
+
+use vars qw(
+            @ISA
+            @EXPORT );
+@ISA =qw(Exporter) ;
+
+@EXPORT = qw( %gmv  );
+
+our  %gmv=(
+	   n=>\&Knight,
+	   r=>\&Rook,
+	   b=>\&Bishop,
+	   q=>\&Queen,
+           p=>\&Pawn,
+	   k=>\&King,
+           u=>\&PawnCatch,
+	   N=>\&Knight,
+	   R=>\&Rook,
+	   B=>\&Bishop,
+	   Q=>\&Queen,
+	   P=>\&Pawn,
+	   K=>\&King,
+           U=>\&PawnCatch) ;
+
+=head2 sub Rook
+
+get the move of the Rook
+
+=cut
+
+sub Rook{
+  my %default=(qw/row 0 col 0 lim 7 dir 3/,
+	       inc_l => [0,0,-1,1],
+	       inc_c=>[-1,1,0,0] ) ;
+  my %arg=@_ ;
+  %arg=(%default,%arg) ;
+
+  lookup(%arg) ;
+
 }
 
-=head2 function2
+=head2 sub Bishop
+
+get the move of the Bishop
 
 =cut
 
-sub function2 {
+sub Bishop{
+my %default=(qw/row 0 col 0 lim 7 dir 3/,
+             inc_l => [1,1,-1,-1],
+             inc_c=>[1,-1,-1,1]) ;
+my %arg=@_ ;
+%arg=(%default,%arg) ;
+
+lookup(%arg) ;
+
+}
+
+=head2 sub Queen
+
+get the move of the Queen
+
+=cut
+
+sub Queen{
+
+my $arr1=[[]] ;
+my %arg=@_ ;
+my $mov=$arg{mov} ;
+
+
+$arg{mov}=$arr1 ;
+Bishop(%arg) ;
+
+foreach (0..3){
+push( @{$mov->[$_]}, @{$arr1->[$_]} ) if (defined( @{$arr1->[$_]})) ;
+}
+
+my $arr2=[[]] ;
+$arg{mov}=$arr2 ;
+Rook(%arg) ;
+foreach (0..3){
+push( @{$mov->[$_+4]}, @{$arr2->[$_]} ) if (defined( @{$arr2->[$_]})) ;
+}
+}
+
+=head2 sub King
+
+get the move of the King
+
+=cut
+
+sub King{
+
+my %default=(lim=>0) ;
+my %arg=@_ ;
+%arg=(%default,%arg) ;
+
+Queen(%arg) ;
+
+
+}
+
+=head2 sub Knight
+
+get the move of the Knight
+
+=cut
+
+sub Knight{
+my %default=(qw/row 0 col 0 lim 0  dir 7/,
+             inc_l => [-2,-2,2,2,-1,1,-1,1],
+             inc_c=>[-1,1,-1,1,-2,-2,2,2] ) ;
+my (%arg)=@_ ;
+%arg=(%default,%arg) ;
+
+lookup(%arg) ;
+
+}
+
+=head2 sub Pawn
+
+get the move of the Pawn
+
+=cut
+
+sub Pawn{
+
+my %default=(qw/row 1 col 0 lim 1  dir 0 color P/,
+             inc_l => [1],
+             inc_c=>[0] ) ;
+my (%arg)=@_ ;
+if($arg{color} eq 'p') {
+$arg{inc_l}=[-1] ;
+$arg{lim}=0 if $arg{row} != 6 ;} 
+else{ $arg{lim}=0 if $arg{row} != 1 ;} 
+%arg=(%default,%arg) ;
+
+lookup(%arg) ;
+return ;
+}
+
+=head2 sub PawnCatch
+
+get the move of the PawnCatch
+
+=cut
+
+sub PawnCatch{
+# can_take + e.p. ?
+my %default=(qw/row 1 col 0 lim 0  dir 1 color U/,
+             inc_l =>[1,1],  #color -1,-1
+             inc_c=>[1,-1] ) ;
+
+my (%arg)=@_ ;
+if($arg{color} eq 'u') {
+$arg{inc_l}=[-1,-1] ;}
+%arg=(%default,%arg) ;
+lookup(%arg) ;
+}
+
+
+
+=head2 sub lookup
+
+generate the move datas
+
+sub lookup{
+
+   row=>'numeric', col =>' numeric , lim=>'limite de mouvement',
+   dir => 'nombre de direction',mov=>'ref avec les cases[dir][]'
+}
+
+=cut
+
+sub lookup{
+
+  my %arg=@_ ;
+
+  my $row=$arg{row} ;
+  my $col=$arg{col} ;
+  my $iter=$arg{lim} ;
+  my $dir=$arg{dir} ;
+  my $mov=$arg{mov} ;
+  my @inc_c=@{$arg{inc_c}} ;
+  my @inc_l=@{$arg{inc_l}} ;
+
+  my $l ;
+  my $c ;
+
+  for (  my $j=0 ; $j<=$dir ; $j++) {
+    my $k=0 ;			
+
+    do  {
+      $l=$col+ $inc_c[$j]*($k+1);
+      $c= $row + $inc_l[$j]*($k+1)  ;
+      $mov->[$j][$k]=chr(ord('a')+$l).($c+1) if ( $l >=0 && $l <=7 && $c >=0 && $c <=7 ) ;
+      $k++ ;
+    }
+      until ($k > $iter || $l <0 || $l > 7 || $c < 0 || $c > 7 ) ;
+  }
 }
 
 =head1 AUTHOR
 
-Charles Minc, C<< <charles.minc at wanadoo.fr> >>
+Charles Minc, C<< <charles.minc@wanadoo.fr> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-chess-chesskit-move at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Chess-ChessKit-ChessKit>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Chess::ChessKit::ChessKit
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Chess-ChessKit-ChessKit>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Chess-ChessKit-ChessKit>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Chess-ChessKit-ChessKit>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Chess-ChessKit-ChessKit>
-
-=back
-
+Please report any bugs or feature requests to
+C<bug-chesskit-board@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=ChessKit>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
 
 =head1 ACKNOWLEDGEMENTS
 
-
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2007 Charles Minc, all rights reserved.
+Copyright 2005 Charles Minc, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
 
-1; # End of Chess::ChessKit::Move
+
+1;
+
+
